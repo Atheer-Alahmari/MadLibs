55d0bd4a37ca0dcd6a6e62321fbe58078a680283
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-14 00:04:46 +0300

    MadLibs

--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+  
     }
 
 
+    
+    @IBAction func navigat(_ sender: Any) {
+        // let destination = sender.source as! ViewController2
+ //        let button = sender as! UIButton
+ //        destination.direc = button.titleLabel?.text
+    }
+    
 }
 
