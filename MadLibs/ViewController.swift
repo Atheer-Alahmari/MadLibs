12e20786165203bc15edd851b92dc99586626d57
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-16 19:44:07 +0300

    New way

--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var setence_Label: UILabel!
+    
+    var sentense = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,13 +21,20 @@ class ViewController: UIViewController {
   
     }
 
-
-    
-    @IBAction func navigat(_ sender: Any) {
-        // let destination = sender.source as! ViewController2
- //        let button = sender as! UIButton
- //        destination.direc = button.titleLabel?.text
+    @IBAction func unwind (_ sender:UIStoryboardSegue){
+        if let destination = sender.source as? ViewController2 {
+            
+        if let adj = destination.adj_TF.text ,
+           let v1 = destination.verb1_TF.text ,
+           let v2 = destination.verb2_TF.text ,
+           let noun = destination.noun_TF.text {
+                
+                setence_Label.text = "We are having a perfectly \(adj) right now. Later we will \(v1) and \(v2) in the \(noun)"
+                
+            }
+        }
     }
+    
     
 }
 
--- MadLibs/ViewController2.swift
+++ MadLibs/ViewController2.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class ViewController2: UIViewController {
 
+    @IBOutlet weak var adj_TF: UITextField!
+    @IBOutlet weak var verb1_TF: UITextField!
+    @IBOutlet weak var verb2_TF: UITextField!
+    @IBOutlet weak var noun_TF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
+  
+   
 
 }
